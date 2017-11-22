8714100e890221edadf60a6547970c06649a10fa
hyperpotamus <nataraja@yandex.ru>
2017-11-22 09:10:20 +0300

    Add files via upload

---
+++ test.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name              Test script
+// @namespace         https://hyperpotamus.github.io/greasemonkey/
+// @description       example script to alert "Hello world!" on somesite.com
+// @include           https://somesite.com/*
+// ==/UserScript==
+
+alert('Hello world!');
