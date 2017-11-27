b067a741d752e2a3e500a320e96dc8b7fc8fa162
hyperpotamus <nataraja@yandex.ru>
2017-11-27 15:04:26 +0300

    Update fadebox.user.js

--- fadebox.user.js
+++ fadebox.user.js
@@ -3,10 +3,11 @@
 // @namespace         https://hyperpotamus.github.io/greasemonkey/
 // @description       change position of fadebox to top
 // @include           https://xcraft.ru/*
+// @include           https://xcraft.net/*
 // @run-at document-end
 // ==/UserScript==
 
 var fb=document.getElementById('fadeBox');
 fb.style.top='0%';
-fb.style.left='700px';
+fb.style.left='750px';
 
