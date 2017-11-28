251a7892893996ad15897ae76ea291e9fdb3c8c2
hyperpotamus <nataraja@yandex.ru>
2017-11-28 19:10:53 +0300

    Update spyblock.user.js

--- spyblock.user.js
+++ spyblock.user.js
@@ -16,22 +16,19 @@ function PageBitHasLoaded (zEvent)
   console.log('spyblock loaded');
 var sbr=document.getElementsByClassName('spy_block_resources');
 if (sbr) {
-var met=parseInt(sbr[0].getElementsByClassName("metal res-icon very-small")[0].innerHTML.replace(/[^\d.]/g, ''), 10);
-var cry=parseInt(sbr[0].getElementsByClassName("crystal res-icon very-small")[0].innerHTML.replace(/[^\d.]/g, ''), 10);
-var gas=parseInt(sbr[0].getElementsByClassName("deuterium res-icon very-small")[0].innerHTML.replace(/[^\d.]/g, ''), 10);
-console.log(met);
-console.log(cry);
-console.log(gas);
-console.log('sum=',met+cry+gas);
-var ressum=met+cry+gas;
-var baklusha=Math.floor(1+ressum/2/4559);
-console.log('need ',baklusha,' baklushas');
-var newElement = document.createElement('div');
-newElement.innerHTML = '<div> ' +
-    'need ' +
-    baklusha +
-    ' baklushas' +
-    '</div>';
-    sbr.parentNode.insertBefore(newElement, sbr.nextSibling);
-  }
+ var met=parseInt(sbr[0].getElementsByClassName("metal res-icon very-small")[0].innerHTML.replace(/[^\d.]/g, ''), 10);
+ var cry=parseInt(sbr[0].getElementsByClassName("crystal res-icon very-small")[0].innerHTML.replace(/[^\d.]/g, ''), 10);
+ var gas=parseInt(sbr[0].getElementsByClassName("deuterium res-icon very-small")[0].innerHTML.replace(/[^\d.]/g, ''), 10);
+ console.log(met);
+ console.log(cry);
+ console.log(gas);
+ console.log('sum=',met+cry+gas);
+ var ressum=met+cry+gas;
+ var baklusha=Math.floor(1+ressum/2/4559);
+ console.log('need ',baklusha,' baklushas');
+ var newElement = document.createElement('div');
+ newElement.innerHTML = '<div> need ' + baklusha + ' baklushas </div>';
+ sbr.parentNode.insertBefore(newElement, sbr.nextSibling);
+ console.log(newElement.innerHTML);
+ }
 }
