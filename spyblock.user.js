8c479e2b941e1353e961465e2a036e83c7c36361
hyperpotamus <nataraja@yandex.ru>
2017-11-28 16:30:37 +0300

    Update spyblock.user.js

--- spyblock.user.js
+++ spyblock.user.js
@@ -7,10 +7,19 @@
 // ==/UserScript==
 
 console.log('spyblock script run');
-var sbr=document.getElementsByClassName('spy_block_resources')[0];
-var met=sbr.getElementsByClassName("metal res-icon very-small")[0].innerHTML;
-var cry=sbr.getElementsByClassName("crystal res-icon very-small")[0].innerHTML;
-var gas=sbr.getElementsByClassName("deuterium res-icon very-small")[0].innerHTML;
+var iover=document.getElementsByClassName('fvk_info box_lb ui_wrapper')[0];
+var dyndiv=iover.getElementsByClassName('container')[0];
+dyndiv.addEvent('load', PageBitHasLoaded);
+
+function PageBitHasLoaded (zEvent)
+{
+  console.log('spyblock loaded');
+var sbr=document.getElementsByClassName('spy_block_resources');
+if (sbr) {
+var met=sbr[0].getElementsByClassName("metal res-icon very-small")[0].innerHTML;
+var cry=sbr[0].getElementsByClassName("crystal res-icon very-small")[0].innerHTML;
+var gas=sbr[0].getElementsByClassName("deuterium res-icon very-small")[0].innerHTML;
 console.log(met);
 console.log(cry);
-console.log(gas);
+console.log(gas);}
+}
